f39a4a9f260f735073bfe8db3073a2982f98bd97
RITIESH VARATHARAJAN <91539605+ritiesh@users.noreply.github.com>
2023-03-04 19:12:20 +0530

    Create Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+void permut(int index,vector<int>&nums,vector<vector<int>>&ans)
+{
+    if(index==nums.size())
+    {
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=index;i<nums.size();i++)
+    {
+        swap(nums[index],nums[i]);
+        permut(index+1,nums,ans);
+        swap(nums[index],nums[i]);
+    }
+}
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        permut(0,nums,ans);
+        return ans;
+    }
+};
